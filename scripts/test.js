f582ea46f28ed5e511e15a7a291ecf908054df99
Manuel Roth <manuelroth@hotmail.ch>
2020-02-11 16:19:37 +0100

    Release 1.2.1

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
-const sass = require('node-sass');
-const jsonImporter = require('node-sass-json-importer');
+const sass = require("sass");
+const jsonImporter = require("node-sass-json-importer");
 
-console.log(sass.renderSync({
-  file: 'scss/general.scss',
-  includePaths: ['scss'],
-  importer: jsonImporter
-}).css.toString());
+console.log(
+  sass
+    .renderSync({
+      file: "scss/general.scss",
+      includePaths: ["scss"],
+      importer: jsonImporter
+    })
+    .css.toString()
+);
